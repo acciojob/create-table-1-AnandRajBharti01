98bb9191c7b638b74199793f4129beaa6c41f4ed
Anand Raj Bharti <anandrajbharti574@gmail.com>
2026-03-31 00:30:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-let c = 1;
 let table = document.getElementById("sampleTable")
 function insert_Row() {
+let c = 1;
     //Write your code here
     let row = document.createElement('tr');
 	let td1 = document.createElement('td');
 	let td2 = document.createElement('td');
 
 	td1.textContent = `New Cell${c}`
-	td2.textContent = `New Cell${c}`
+	td2.textContent = `New Cell${c+1}`
 	c+=1;
 
 	row.prepend(td1,td2);
