f98d7be623c11cfbae40bcb7accc36a07d99547e
Anand Raj Bharti <anandrajbharti574@gmail.com>
2026-05-11 23:28:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
-let table = document.getElementById("sampleTable")
-let count = 1;
+let table = document.getElementById("sampleTable");
+
 function insert_Row() {
-// let c = 1;
-    //Write your code here
-    let row = document.createElement('tr');
-	let td1 = document.createElement('td');
-	let td2 = document.createElement('td');
-	
-	td1.textContent = `New${count} Cell1`
-	td2.textContent = `New${count} Cell2`
-		// count++;
-	// c+=1;
 
-	row.append(td1);
-	row.append(td2);
-	// table.prepend(row)
+    let row = document.createElement("tr");
 
- table.insertBefore(row, table.firstChild);
-	count++;
-	
-	
+    let td1 = document.createElement("td");
+    let td2 = document.createElement("td");
+
+    td1.innerHTML = "New Cell1";
+    td2.innerHTML = "New Cell2";
+
+    row.appendChild(td1);
+    row.appendChild(td2);
+
+    table.insertBefore(row, table.firstChild);
 }
